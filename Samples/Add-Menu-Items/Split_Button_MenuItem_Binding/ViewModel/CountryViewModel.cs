43fe496ad87bbe8c7bfca840ec3c2b2c3c076018
Mageshyadav M <44959216+mageshyadavm@users.noreply.github.com>
2020-04-15 08:57:50 +0530

    New Sample Added

---
+++ Samples/Add-Menu-Items/Split_Button_MenuItem_Binding/ViewModel/CountryViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Split_Button_MenuItem_Binding
+{
+    class CountryViewModel
+    {
+        private List<Country> dropDownItems;
+
+        public List<Country> DropDownItems
+        {
+            get
+            {
+                return dropDownItems;
+            }
+            set
+            {
+                dropDownItems = value;
+            }
+        }
+
+        public CountryViewModel()
+        {
+            DropDownItems = new List<Country>();
+            DropDownItems.Add(new Country()
+            {
+                Name = "India",
+                Flag = new BitmapImage(new Uri("/Images/india.png", UriKind.RelativeOrAbsolute))
+            });
+            DropDownItems.Add(new Country()
+            {
+                Name = "France",
+                Flag = new BitmapImage(new Uri("/Images/france.png", UriKind.RelativeOrAbsolute))
+            });
+            DropDownItems.Add(new Country()
+            {
+                Name = "Germany",
+                Flag = new BitmapImage(new Uri("/Images/germany.png", UriKind.RelativeOrAbsolute))
+            });
+        }
+    }
+}
