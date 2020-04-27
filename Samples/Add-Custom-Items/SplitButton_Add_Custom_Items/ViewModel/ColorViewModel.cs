019a3c1a39b45bb8df1c239a7e7cf4dcadfa6e68
Mageshyadav M <44959216+mageshyadavm@users.noreply.github.com>
2020-04-27 05:37:17 +0530

    Added Custom Items Sample

---
+++ Samples/Add-Custom-Items/SplitButton_Add_Custom_Items/ViewModel/ColorViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Syncfusion.Windows.Shared;
+using Syncfusion.Windows.Tools.Controls;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace SplitButton_Custom_Items
+{
+    public class ColorViewModel : NotificationObject
+    {
+        public ObservableCollection<UIElement> color = new ObservableCollection<UIElement>();
+        public ObservableCollection<UIElement> items = new ObservableCollection<UIElement>();
+
+        public ObservableCollection<UIElement> Colors
+        {
+            get { return color; }
+            set { color = value; RaisePropertyChanged("Colors"); }
+        }
+
+        public ObservableCollection<UIElement> Items
+        {
+            get { return items; }
+            set { items = value; RaisePropertyChanged("Items"); }
+        }
+
+
+        public ColorViewModel()
+        {
+            Colors.Add(new DropDownMenuItem() { Header = "More Items", Icon = new Image() { Source = new BitmapImage(new Uri("/Images/skyblue.png", UriKind.RelativeOrAbsolute)) } });
+            Items.Add(new Label() { Content = "More Items" });
+        }
+    }
+}
